309d77c60541d456253c802c637c438af7a60c76
Karen Celis <47069950+KarenCelis@users.noreply.github.com>
2020-08-13 15:28:17 -0500

    Numero Par

--- NumeroPar/public_html/js/index.js
+++ NumeroPar/public_html/js/index.js
@@ -4,4 +4,21 @@
  * and open the template in the editor.
  */
 
+document.write("Programa para determinar si un numero es par:<br/>");
+/*REcibir un parametro asignarlo y convertir un String a un numero entero*/
+var num = parseInt(prompt("Proporciona un numero:", 1));
 
+if (num >= 0) {
+
+    if (num % 2 == 0) {
+        document.write("El numero es par<br/>");
+
+    } else {
+        document.write("El numero no es par<br/>");
+
+    }
+} else {
+    document.write("Prporcione uin valor mayor a 0<br/>");
+
+}
+document.write("el valor que escribio fue " + num);
